0b460d1430d6c9a1c1aa9587f92a067661e56fcf
Shaun Tucker <tucke@Shauns-MacBook-Air.local>
2018-01-06 12:06:45 -0500

    View Complete with sideview blur effect

--- PhaseOne2018/ViewController.swift
+++ PhaseOne2018/ViewController.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var viewConstraint: NSLayoutConstraint!
+    @IBOutlet var blurView: UIVisualEffectView!
+    @IBOutlet var sideView: UIView!
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        blurView.layer.cornerRadius = 15
+        sideView.layer.shadowColor = UIColor.black.cgColor
+        sideView.layer.shadowOpacity = 1
+        sideView.layer.shadowOffset = CGSize(width: 5, height: 0)
+        
+        button1.layer.shadowColor = UIColor.black.cgColor
+        button1.layer.shadowOpacity = 0.3
+        button1.layer.shadowOffset = CGSize(width: 0.3 , height: 0)
+        
+        button2.layer.shadowColor = UIColor.black.cgColor
+        button2.layer.shadowOpacity = 0.3
+        button2.layer.shadowOffset = CGSize(width: 0.3 , height: 0)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
+    
 
 
 }
